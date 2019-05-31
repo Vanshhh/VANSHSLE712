1a62c3d7e96256624586e0734efe66b3636c631d
markziemann <mark.ziemann@gmail.com>
2019-05-31 13:24:57 +1000

    update

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+COUNT=$1
+
+echo $COUNT
+
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa 
 
 
-msbar -sequence 2.fa -count 450 -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
+msbar -sequence 2.fa -count $COUNT -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
 
 
 blast2 -p blastn -e 0.001 -m 8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa
